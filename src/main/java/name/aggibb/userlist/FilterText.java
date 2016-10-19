2743e1c9736ae490ba7d4ef7740886c5de511ca0
Andy Gibb <aggibb@gmail.com>
2016-10-19 16:58:36 -0700

    Rename field variable to nameText to avoid namespace confusion

--- src/main/java/name/aggibb/userlist/FilterText.java
+++ src/main/java/name/aggibb/userlist/FilterText.java
@@ -1,17 +1,19 @@
 package name.aggibb.userlist;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.NotEmpty;
 
 /**
  * Created by agibb on 2016-10-06.
  */
 @Data
 public class FilterText {
-    private String filterText;
+    @NotEmpty
+    private String nameText;
 
     public FilterText(){}
 
-    public FilterText(String filterText) {
-        this.filterText = filterText;
+    public FilterText(String nameText) {
+        this.nameText = nameText;
     }
 }
